6a3833bc66b8eefbc3fd8b887db7c8d1240d6c7d
Hildana-ui <hildana.amare@bitscollege.edu.et>
2025-12-09 16:26:20 +0300

    solved calculator problem

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(a , b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (i = 0; i<arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let product =  1;
+  for (i = 0; i<arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const multiply = function() {
-
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 || n === 1){
+    return 1;
+  }
+  return n * factorial(n-1);
+  
 };
 
 // Do not edit below this line
