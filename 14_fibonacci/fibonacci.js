7b415842159d13fb4381e953077ea6665f8e25bf
Hildana-ui <hildana.amare@bitscollege.edu.et>
2025-12-09 19:23:47 +0300

    solved fibonacci problem

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num = parseInt(n);
+    if(num === 0) {
+        return 0;
+    }
+    if(num === 1) {
+        return 1;
+    } 
+    if (num < 0) {
+        return "OOPS";
+    }else {
+        return fibonacci(num-1) + fibonacci(n-2);
+    }
 };
 
 // Do not edit below this line
